cad72579a0cb987d89e0b6ff89ba4c75d7ba1c65
Vasudha Rana <23bcs11284@cuchd.in>
2026-06-05 12:00:03 +0530

    Day 3 setup ready

--- backend/lms/lms/src/main/java/com/example/lms/controller/studentController.java
+++ backend/lms/lms/src/main/java/com/example/lms/controller/studentController.java
@@ -1,16 +1,22 @@
 package com.example.lms.controller;
 import com.example.lms.model.Student;
+
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/students")
 @CrossOrigin("*")
 public class StudentController{
     @GetMapping
-    public Student getStudent() {
-        return new Student(
-                1,
-                "diya",
-                "cse"
-        );
+    public ArrayList<Student> getStudent() {
+       ArrayList<Student> students=new ArrayList<>();
+       students.add(
+              new Student(1,"Diya","CSE")
+       );
+       students.add(
+              new Student(2,"Vasudha","BE-CSE")
+       );
+       return students;
     }
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App()
 {
     // {"id":1,"name":"diya","course":"cse"}
-    const[ students,setStudents]=useState({}); //used to store data
+    const[ students,setStudents]=useState([]); //used to store data
 
     const getStudentData=async()=>{
         const response=await fetch('http://localhost:8080/students');
@@ -16,9 +16,13 @@ function App()
                 Get Student data
             </button>
             <h1>This is the student data:</h1>
-            <h2>{students.name}</h2>
-            <h2>{students.id}</h2>
-            <h2>{students.course}</h2>
+            <ul>
+                {students.map(Student=>(
+                    <li Key={Student.id}>
+                        {Student.name}.{Student.course}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
