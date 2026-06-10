8e495f6b214d485fa2e5c44402982aced5766ffa
Vasudha Rana <23bcs11284@cuchd.in>
2026-06-10 12:27:33 +0530

    Day 7 hibernate and jpa completed

--- backend/lms/lms/src/main/java/com/example/lms/controller/studentController.java
+++ backend/lms/lms/src/main/java/com/example/lms/controller/studentController.java
@@ -2,29 +2,26 @@ package com.example.lms.controller;
 import com.example.lms.model.Student;
 import com.example.lms.service.StudentService;
 
-import java.util.ArrayList;
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/students")
 @CrossOrigin("*")
 public class StudentController{
        @Autowired
-       private StudentService service;
-       @Autowired
-       JdbcTemplate jdbcTemplate;
-    @GetMapping
-    public ArrayList<Student> getStudent() {
-       ArrayList<Student> students=new ArrayList<>();
-       students.add(
-              new Student(1,"Diya","CSE")
-       );
-       students.add(
-              new Student(2,"Vasudha","BE-CSE")
-       );
-       return students;
-    }
+       private  StudentService service;
+   //  @GetMapping
+   //  public ArrayList<Student> getStudent() {
+   //     ArrayList<Student> students=new ArrayList<>();
+   //     students.add(
+   //            new Student(1,"Diya","CSE")
+   //     );
+   //     students.add(
+   //            new Student(2,"Vasudha","BE-CSE")
+   //     );
+   //     return students;
     @GetMapping("/count")
      public String  countStudents() {
          return service.getStudentCount();
@@ -33,4 +30,12 @@ public class StudentController{
     public String getMessage(){
        return service.getMessage();
     }
+    @GetMapping
+     public List<Student> getAllStudents(){
+            return service.getAllStudents();
+        }
+        @PostMapping
+public Student addStudent(@RequestBody Student student) {
+    return service.saveStudent(student);
+}
 }
--- backend/lms/lms/src/main/java/com/example/lms/service/StudentService.java
+++ backend/lms/lms/src/main/java/com/example/lms/service/StudentService.java
@@ -1,6 +1,13 @@
 package com.example.lms.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.lms.model.Student;
+import com.example.lms.repository.StudentRepository;
+
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 @Service
 public class StudentService {
@@ -9,6 +16,8 @@ public class StudentService {
     }
     @Autowired //Di-FIlled injecttion
      private JdbcTemplate jdbcTemplate;
+     @Autowired
+      private StudentRepository repository;
     public String getStudentCount(){
         
         String sql = "SELECT COUNT(*) FROM students";
@@ -18,4 +27,11 @@ public class StudentService {
                 Integer.class
         ).toString();
     }
+        public List<Student> getAllStudents(){
+            return repository.findAll();
+        }
+        public Student saveStudent( Student student) {
+    return repository.save(student);
+}
+    
 }
